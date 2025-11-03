0bd35d964f6b6dc17ce058b81dbd7a6e5f366e81
mikjimenez <mik.jimenez@duocuc.cl>
2025-11-03 04:06:28 -0300

    Segundo cambio

--- app/src/main/java/com/example/modaurbana/app/ui/theme/Color.kt
+++ app/src/main/java/com/example/modaurbana/app/ui/theme/Color.kt
@@ -5,7 +5,7 @@ import androidx.compose.ui.graphics.Color
 // Colores principales (Moda Urbana - Verde sostenible)
 
 // En modo oscuro consume estos colores 
-val Green80 = Color(0xFF81C784)
+val Green80 = Color(0xFF28A12D)
 val GreenGrey80 = Color(0xFFA5D6A7)
 val Mint80 = Color(0xFFB2DFDB)
 
